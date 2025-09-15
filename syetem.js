c3f2829fee66ea1de0e7555296bd620f12b89188
Harun <mdh574349@gmail.com>
2025-09-16 00:16:53 +0600

    copy button chara baki sob complete

--- syetem.js
+++ syetem.js
@@ -1,5 +1,5 @@
 let y=parseInt(document.getElementById('coin-count').innerText);
-y=50;
+y=110;
 document.getElementById('coin-count').innerText=y;
 
 
@@ -28,6 +28,23 @@ document.getElementById('call-emergency').addEventListener('click', function() {
         alert("Calling Emergency Services...");
         y -= 20;
         document.getElementById('coin-count').innerText = y;
+        // Creating a new card element
+        const now = new Date(); 
+        const dateTime = now.toLocaleString(); 
+             const container = document.getElementById('cards-container');
+
+            const newCard = document.createElement('div');
+            newCard.classList.add('emergency-number-called', 'flex', 'justify-between', 'items-center', 'border', 'p-5', 'm-5');
+            newCard.innerHTML = `
+                <div class="left">
+                    <h2>National Emergency Number</h2>
+                    <p>999</p>
+                </div>
+                <div class="right">
+                ${dateTime}
+                </div>
+            `;
+            container.appendChild(newCard);
     }
     else{
         alert("Not enough coins to make the call.");
@@ -44,6 +61,25 @@ document.getElementById('call-police').addEventListener('click', function() {
         alert("Calling Police...");
         y -= 20;
         document.getElementById('coin-count').innerText = y;
+        
+        // Creating a new card element
+        //getting date first
+        const now = new Date(); 
+        const dateTime = now.toLocaleString(); 
+             const container = document.getElementById('cards-container');
+
+            const newCard = document.createElement('div');
+            newCard.classList.add('police-called', 'flex', 'justify-between', 'items-center', 'border', 'p-5', 'm-5');
+            newCard.innerHTML = `
+                <div class="left">
+                    <h2>Police Helpline Number</h2>
+                    <p>999</p>
+                </div>
+                <div class="right">
+                ${dateTime}
+                </div>
+            `;
+            container.appendChild(newCard);
     }
     else{
         alert("Not enough coins to make the call.");
@@ -58,6 +94,24 @@ document.getElementById('fire-service-calling').addEventListener('click', functi
         alert("Calling FireService...");
         y -= 20;
         document.getElementById('coin-count').innerText = y;
+         // Creating a new card element
+        //getting date first
+        const now = new Date(); 
+        const dateTime = now.toLocaleString(); 
+             const container = document.getElementById('cards-container');
+
+            const newCard = document.createElement('div');
+            newCard.classList.add('fire-service-called', 'flex', 'justify-between', 'items-center', 'border', 'p-5', 'm-5');
+            newCard.innerHTML = `
+                <div class="left">
+                    <h2>Fire-Service Helpline Number</h2>
+                    <p>999</p>
+                </div>
+                <div class="right">
+                ${dateTime}
+                </div>
+            `;
+            container.appendChild(newCard);
     }
     else{
         alert("Not enough coins to make the call.");
@@ -65,3 +119,214 @@ document.getElementById('fire-service-calling').addEventListener('click', functi
 
 })
 //call fire service button functionality ends here
+
+
+
+
+//Ambulance service button functionality starts here
+
+document.getElementById('ambulence-calling-btn').addEventListener('click', function() {
+    if(y>=20){
+        alert("Calling Ambulence...");
+        y -= 20;
+        document.getElementById('coin-count').innerText = y;
+         // Creating a new card element
+        //getting date first
+        const now = new Date(); 
+        const dateTime = now.toLocaleString(); 
+             const container = document.getElementById('cards-container');
+
+            const newCard = document.createElement('div');
+            newCard.classList.add('ambulence-service-called', 'flex', 'justify-between', 'items-center', 'border', 'p-5', 'm-5');
+            newCard.innerHTML = `
+                <div class="left">
+                    <h2>Ambulence Service </h2>
+                    <p>1994-999999</p>
+                </div>
+                <div class="right">
+                ${dateTime}
+                </div>
+            `;
+            container.appendChild(newCard);
+    }
+    else{
+        alert("Not enough coins to make the call.");
+    }
+
+})
+
+
+
+
+//W&C help button functionality starts here
+document.getElementById('w&c-help-btn').addEventListener('click', function() {
+    if(y>=20){
+        alert("Women child helping calling...");
+        y -= 20;
+        document.getElementById('coin-count').innerText = y;
+         // Creating a new card element
+        //getting date first
+        const now = new Date(); 
+        const dateTime = now.toLocaleString(); 
+             const container = document.getElementById('cards-container');
+
+            const newCard = document.createElement('div');
+            newCard.classList.add('ambulence-service-called', 'flex', 'justify-between', 'items-center', 'border', 'p-5', 'm-5');
+            newCard.innerHTML = `
+                <div class="left">
+                    <h2>Women & Child Helpline </h2>
+                    <p>109</p>
+                </div>
+                <div class="right">
+                ${dateTime}
+                </div>
+            `;
+            container.appendChild(newCard);
+    }
+    else{
+        alert("Not enough coins to make the call.");
+    }
+
+})
+//W&C help button functionality Ends here
+
+
+//Anti-corruption helpline button functionality starts here
+
+
+document.getElementById('dudok-call-btn').addEventListener('click', function() {
+    if(y>=20){
+        alert("Dudok calling...");
+        y -= 20;
+        document.getElementById('coin-count').innerText = y;
+         // Creating a new card element
+        //getting date first
+        const now = new Date(); 
+        const dateTime = now.toLocaleString(); 
+             const container = document.getElementById('cards-container');
+
+            const newCard = document.createElement('div');
+            newCard.classList.add('ambulence-service-called', 'flex', 'justify-between', 'items-center', 'border', 'p-5', 'm-5');
+            newCard.innerHTML = `
+                <div class="left">
+                    <h2>Anti-Corruption Helpline </h2>
+                    <p>106</p>
+                </div>
+                <div class="right">
+                ${dateTime}
+                </div>
+            `;
+            container.appendChild(newCard);
+    }
+    else{
+        alert("Not enough coins to make the call.");
+    }
+
+})
+//Anti-corruption helpline button functionality Ends here
+
+
+//Electricity helpline button functionality starts here
+document.getElementById('karenter-mistiri-btn').addEventListener('click', function() {
+    if(y>=20){
+        alert("Electricity helpline calling...");
+        y -= 20;
+        document.getElementById('coin-count').innerText = y;
+         // Creating a new card element
+        //getting date first
+        const now = new Date(); 
+        const dateTime = now.toLocaleString(); 
+             const container = document.getElementById('cards-container');
+
+            const newCard = document.createElement('div');
+            newCard.classList.add('ambulence-service-called', 'flex', 'justify-between', 'items-center', 'border', 'p-5', 'm-5');
+            newCard.innerHTML = `
+                <div class="left">
+                    <h2>Electricity Helpline</h2>
+                    <p>16216</p>
+                </div>
+                <div class="right">
+                ${dateTime}
+                </div>
+            `;
+            container.appendChild(newCard);
+    }
+    else{
+        alert("Not enough coins to make the call.");
+    }
+
+})
+//electricity helpline button functionality Ends here
+
+
+// Brack Helpline starts here
+document.getElementById('brac-calling-btn').addEventListener('click', function() {
+    if(y>=20){
+        alert("Brac calling...");
+        y -= 20;
+        document.getElementById('coin-count').innerText = y;
+         // Creating a new card element
+        //getting date first
+        const now = new Date(); 
+        const dateTime = now.toLocaleString(); 
+             const container = document.getElementById('cards-container');
+
+            const newCard = document.createElement('div');
+            newCard.classList.add('ambulence-service-called', 'flex', 'justify-between', 'items-center', 'border', 'p-5', 'm-5');
+            newCard.innerHTML = `
+                <div class="left">
+                    <h2>BracHelpline</h2>
+                    <p>16445</p>
+                </div>
+                <div class="right">
+                ${dateTime}
+                </div>
+            `;
+            container.appendChild(newCard);
+    }
+    else{
+        alert("Not enough coins to make the call.");
+    }
+
+})
+//Brack Helpline ends here
+
+
+// Railway button functionality starts here
+document.getElementById('railway-calling-btn').addEventListener('click', function() {
+    if(y>=20){
+        alert("Railway calling...");
+        y -= 20;
+        document.getElementById('coin-count').innerText = y;
+         // Creating a new card element
+        //getting date first
+        const now = new Date(); 
+        const dateTime = now.toLocaleString(); 
+             const container = document.getElementById('cards-container');
+
+            const newCard = document.createElement('div');
+            newCard.classList.add('ambulence-service-called', 'flex', 'justify-between', 'items-center', 'border', 'p-5', 'm-5');
+            newCard.innerHTML = `
+                <div class="left">
+                    <h2>Railway Helpine</h2>
+                    <p>163</p>
+                </div>
+                <div class="right">
+                ${dateTime}
+                </div>
+            `;
+            container.appendChild(newCard);
+    }
+    else{
+        alert("Not enough coins to make the call.");
+    }
+
+})
+//Railway button functionality Ends here
+
+
+
+
+
+
+
